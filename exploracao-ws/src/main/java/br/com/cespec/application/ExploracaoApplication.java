bbbc72ec7fc0953307306a0093f0193a43519eae
msantocardoso <msantocardoso@gmail.com>
2015-07-01 21:31:09 -0300

    Estrutura inicial do projeto.

---
+++ exploracao-ws/src/main/java/br/com/cespec/application/ExploracaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.cespec.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploracaoApplication.class, args);
+	}
+}
---
+++ exploracao-ws/src/main/java/br/com/cespec/application/infra/configuration/WebConfiguration.java
@@ -0,0 +1,46 @@
+package br.com.cespec.application.infra.configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+	@Bean
+	public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		objectMapper.enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS);
+		objectMapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"));
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+
+		jsonConverter.setObjectMapper(objectMapper);
+
+		return jsonConverter;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+		converters.add(customJackson2HttpMessageConverter());
+
+		super.addDefaultHttpMessageConverters(converters);
+	}
+}
